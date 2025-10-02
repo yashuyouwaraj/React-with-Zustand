ab7e11d04fbf20b7b74571a47ca1f880d6984681
Yashu Youwaraj <amanyasu@gmail.com>
2025-10-02 18:55:42 +0530

    RecipeBook

---
+++ Receipie App/src/App.tsx
@@ -0,0 +1,12 @@
+import ReceipeBook from "./components/ReceipeBook"
+
+
+const App = () => {
+  return (
+    <div>
+      <ReceipeBook />
+    </div>
+  )
+}
+
+export default App
---
+++ Receipie App/src/store/store.ts
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+
+interface Recipe {
+    id:number;
+    name:string;
+    ingredients:string[];
+    instructions:string;
+}
+
+interface RecipeStore{
+    recipes:Recipe[];
+    addRecipe: (recipe:Recipe)=>void;
+    removeRecipe:(id:number)=>void;
+}
+
+export const useStore = create<RecipeStore>((set)=>({
+    recipes:[],
+    addRecipe :(recipe)=>set((state)=>({
+        recipes: [...state.recipes,recipe]
+    })),
+    removeRecipe:(id)=>set((state)=>({
+        recipes:state.recipes.filter((recipe)=>recipe.id!==id)
+    }))
+}))
